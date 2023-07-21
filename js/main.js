b621de9a5367dde108b0aa3a2f9d53b744bbfd5e
ShajjadKhan <shajjadkhan.me@gmail.com>
2023-07-22 00:15:41 +0600

    without reload response

--- js/main.js
+++ js/main.js
@@ -23,7 +23,71 @@ closeBtn.onclick = () =>{
 
 
 const preloader = document.querySelector(".preloader");
+const arrowBtn = document.querySelector(".arrowBtn");
+
 
 window.addEventListener("load", (event) => {
     preloader.classList.add("active");
   });
+
+
+//   scroll document start
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+  arrowBtn.classList.add("activeArrowBtn");
+
+  }
+  
+  
+  else{
+    arrowBtn.classList.remove("activeArrowBtn")
+  }
+}
+
+
+// without reload request and response start 
+function  loadContact(){
+  // create new request start
+  const xhr = new XMLHttpRequest ();
+  // create new request end 
+
+  // response  aarrives
+  xhr.onload = function(){
+    const contactContainer = document.querySelector("body");
+    contactContainer.innerHTML = xhr.responseText;
+  }
+// prepaare request 
+
+  xhr.open("GET", "contact.html");
+
+  // send request 
+  xhr.send();
+
+}
+
+// view blog without reload 
+function blogCard(){
+  const xhrblog = new XMLHttpRequest ();
+
+   // response  aarrives
+   xhrblog.onload = function(){
+    const blogContainer = document.querySelector("body");
+    blogContainer.innerHTML = xhrblog.responseText;
+  }
+
+
+  // prepaare request 
+
+  xhrblog.open("GET", "view.html");
+
+  // send request 
+  xhrblog.send();
+}
+
+// without reload request and response end 
+
+
+//   scroll document end
+
